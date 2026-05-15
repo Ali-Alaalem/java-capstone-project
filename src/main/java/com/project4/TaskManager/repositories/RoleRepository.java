86bb36955b94d6b761960694aee5f9d39470f1fd
Ali-Alaalem <meemo2002a@gmail.com>
2026-05-15 12:21:24 +0300

    adding RoleRepository

---
+++ src/main/java/com/project4/TaskManager/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.project4.TaskManager.repositories;
+
+import com.project4.TaskManager.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
